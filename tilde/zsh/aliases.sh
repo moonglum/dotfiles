a288249be1d027ff916380bfeaea6ba652626a3f
moonglum <moonglum@moonbeamlabs.com>
2014-01-23 21:28:08 +0100

    Refactored Jump

--- tilde/zsh/aliases.sh
+++ tilde/zsh/aliases.sh
@@ -55,3 +55,20 @@ alias c='echo "Use j c"'
 alias n='echo "Use j n"'
 alias dropbox='echo "Use j dropbox"'
 alias desktop='echo "Use j desktop"'
+
+# Jump
+# Inspired by the Oh-My-ZSH Jump Plugin
+export MARKPATH=$HOME/.marks
+
+# jump FOO: jump to a mark named FOO
+jump() { cd -P "$MARKPATH/$1" }
+_jump() { _files -W "$MARKPATH/$1" }
+compdef _jump jump
+
+# mark FOO: create a mark named FOO
+mark() { mkdir -p "$MARKPATH"; ln -s "$(pwd)" "$MARKPATH/$1" }
+
+# unmark FOO: delete a mark
+unmark() { rm -i "$MARKPATH/$1" }
+
+# marks: lists all marks (see: functions)
