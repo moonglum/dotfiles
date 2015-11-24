7d48f7053a9e7448ab3782c546a4d30db29a14a9
Lucas Dohmen <lucas@dohmen.io>
2015-11-24 10:45:08 +0100

    A little bit of cleanup of my path
    There was still some Mac-y stuff in there. Now everything is so clean.

--- tilde/zsh/path.sh
+++ tilde/zsh/path.sh
@@ -5,22 +5,3 @@
 
 # Home
 PATH=$PATH:$HOME/.bin
-
-# For homebrew
-PATH=/usr/local/bin:$PATH
-PATH=/usr/local/sbin:$PATH
-
-# For LaTeX
-PATH=/usr/texbin:$PATH
-
-# For Cabal
-PATH=/Users/moonglum/.cabal/bin:$PATH
-
-# For NPM
-PATH=/usr/local/nodejs/default/bin:$PATH
-
-# For Heroku Toolbelt
-PATH=/usr/local/heroku/bin:$PATH
-
-# For Chicken
-PATH=/usr/local/Cellar/chicken/4.8.0.5/bin:$PATH
