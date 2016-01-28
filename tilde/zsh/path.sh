44a3f8c6fac2c0806edd965d839eb2047642ff26
Lucas Dohmen <lucas@dohmen.io>
2016-01-28 11:38:47 +0100

    tmuxinator works just fine without this line oO
    And this scripts directory didn't exist anyway oO

--- tilde/zsh/path.sh
+++ tilde/zsh/path.sh
@@ -1,7 +1,4 @@
 ## Manipulation of the path and loading stuff
 
-# tmuxinator
-[[ -s $HOME/.tmuxinator/scripts/tmuxinator ]] && source $HOME/.tmuxinator/scripts/tmuxinator
-
 # Home
 PATH=$PATH:$HOME/.bin
