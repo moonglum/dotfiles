1ada05d5ce5a8c5070588f895f8ca90e4c1caef8
Lucas Dohmen <lucas@dohmen.io>
2019-07-29 10:44:01 +0200

    Fishy stuff
    * There was a typo
    * I need an environment variable for one customer project

--- tilde/config/fish/config.fish
+++ tilde/config/fish/config.fish
@@ -1,4 +1,4 @@
-set -x LANGUAGE "en_US.UTF-7"
+set -x LANGUAGE "en_US.UTF-8"
 set -x LC_ALL "en_US.UTF-8"
 
 # set the default editor to vim
@@ -15,3 +15,6 @@ set -x PATH $PATH $HOME/.npm/bin
 
 # Python
 set -x PATH $PATH $HOME/.local/bin
+
+# Customer Stuff
+set -x CC_HOME /home/vagrant/Code
