e8dfd92d4498b713cacffa8e273e5520ef0a83d1
Lucas Dohmen <lucas@dohmen.io>
2022-05-11 16:31:08 +0200

    Remove asdf stuff

--- tilde/config/fish/config.fish
+++ tilde/config/fish/config.fish
@@ -7,23 +7,10 @@ set -x EDITOR 'vim'
 # use fdfind as the file list for fzf
 set -x FZF_DEFAULT_COMMAND "fdfind --type file"
 
-# Ruby
-# TODO: This doesn't make sense, this should be handled by asdf
-set -x PATH $HOME/.gem/ruby/3.1.0/bin $PATH
-
-# Python
-set -x PATH $HOME/.local/bin $PATH
-
 # Go
 set -x GOPRIVATE bitbucket.org/advance52
 set -x GOPATH $HOME/.go
 set -x PATH $HOME/.go/bin $PATH
-
-# Rust
-set -x PATH $HOME/.cargo/bin $PATH
-
-# asdf
-# source ~/.asdf/asdf.fish
 
 # direnv
 eval (direnv hook fish)
